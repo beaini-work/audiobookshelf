fa2cf91acc2a60be97588c37df11bcfa04f2422d
beaini-work <work@beaini.ca>
2025-02-26 17:34:29 +0000

    Handle Null Timestamp in Voice Chat Modal Formatting

--- client/components/modals/VoiceChatModal.vue
+++ client/components/modals/VoiceChatModal.vue
@@ -541,6 +541,7 @@ export default {
     },
 
     formatTimestamp(timestamp) {
+      if (!timestamp) return ''
       return timestamp.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' })
     },
 
